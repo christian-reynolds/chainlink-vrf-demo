7d2f277d5aa1dfa9e1d65ca667002dccf1671ed9
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2021-12-28 05:33:50 -0500

    Updated deploy script

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Randomness = await hre.ethers.getContractFactory("Randomness");
+  const randomness = await Randomness.deploy();
 
-  await greeter.deployed();
+  await randomness.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Randomness deployed to:", randomness.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
